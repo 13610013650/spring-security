c34611b86dfdc13d4de80ad67c7789554fc3a925
zhangsheng_global <zhangsheng_global@qq.com>
2019-10-04 17:45:42 +0800

    frist project

---
+++ spring-security-jwt-guide/src/main/java/github/javaguide/springsecurityjwtguide/security/utils/JwtTokenUtils.java
@@ -0,0 +1,77 @@
+package github.javaguide.springsecurityjwtguide.security.utils;
+
+import github.javaguide.springsecurityjwtguide.security.constants.SecurityConstants;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import javax.crypto.SecretKey;
+import javax.xml.bind.DatatypeConverter;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author shuang.kou
+ */
+public class JwtTokenUtils {
+
+    /**
+     * 角色的key
+     **/
+    private static final String ROLE_CLAIMS = "rol";
+    /**
+     * rememberMe 为 false 的时候过期时间是1个小时
+     */
+    private static final long EXPIRATION = 60 * 60L;
+    /**
+     * rememberMe 为 true 的时候过期时间是7天
+     */
+    private static final long EXPIRATION_REMEMBER = 60 * 60 * 24 * 7L;
+
+    /**
+     * 生成足够的安全随机密钥，以适合符合规范的签名
+     */
+    private static byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(SecurityConstants.JWT_SECRET_KEY);
+    private static SecretKey secretKey = Keys.hmacShaKeyFor(apiKeySecretBytes);
+
+    public static String createToken(String username, List<String> roles, boolean isRememberMe) {
+        long expiration = isRememberMe ? EXPIRATION_REMEMBER : EXPIRATION;
+
+        String tokenPrefix = Jwts.builder()
+                .setHeaderParam("typ", SecurityConstants.TOKEN_TYPE)
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .claim(ROLE_CLAIMS, String.join(",", roles))
+                .setIssuer("SnailClimb")
+                .setIssuedAt(new Date())
+                .setSubject(username)
+                .setExpiration(new Date(System.currentTimeMillis() + expiration * 1000))
+                .compact();
+        return SecurityConstants.TOKEN_PREFIX + tokenPrefix;
+    }
+
+    public static String getUsernameByToken(String token) {
+        return getTokenBody(token).getSubject();
+    }
+
+    /**
+     * 获取用户所有角色
+     */
+    public static List<SimpleGrantedAuthority> getUserRolesByToken(String token) {
+        String role = (String) getTokenBody(token)
+                .get(ROLE_CLAIMS);
+        return Arrays.stream(role.split(","))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    private static Claims getTokenBody(String token) {
+        return Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
